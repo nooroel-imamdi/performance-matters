2927b672e6a93bdd0be69e23cc1c192080e7e488
Nooroel Imamdi <nooroelimamdi@gmail.com>
2017-03-16 23:08:28 +0100

    hulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     criticalCss = require('gulp-critical-css'),
     imagemin = require('gulp-imagemin'),
-    webp = require('gulp-webp');
+    webp = require('gulp-webp'),
+    cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', () => {
 gulp.src('src/file.css')
@@ -9,14 +10,20 @@ gulp.src('src/file.css')
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('images', function(){
-  gulp.src('src/assets/img/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/img/'));
-});
+// gulp.task('images', function(){
+//   gulp.src('src/assets/img/**/*')
+//     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+//     .pipe(gulp.dest('dist/img/'));
+// });
 
 gulp.task('default', function () {
-    return gulp.src('src/assets/img/**.jpg')
+    return gulp.src('src/assets/img/**/*')
         .pipe(webp())
         .pipe(gulp.dest('src/assets/img/webp'));
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('src/assets/css/src/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/min'));
 });
