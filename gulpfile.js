726a04b208d710bfe275d1cd5bb4a558c0322503
Nooroel Imamdi <nooroelimamdi@gmail.com>
2017-03-17 12:53:20 +0100

    gulp-image

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@ var gulp = require('gulp'),
     criticalCss = require('gulp-critical-css'),
     imagemin = require('gulp-imagemin'),
     webp = require('gulp-webp'),
-    cleanCSS = require('gulp-clean-css');
+    cleanCSS = require('gulp-clean-css'),
+    concatCss = require('gulp-concat-css'),
+    concatJS = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    image = require('gulp-image');
 
 gulp.task('default', () => {
 gulp.src('src/file.css')
@@ -10,20 +15,39 @@ gulp.src('src/file.css')
     .pipe(gulp.dest('dist'))
 });
 
-// gulp.task('images', function(){
-//   gulp.src('src/assets/img/**/*')
-//     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-//     .pipe(gulp.dest('dist/img/'));
-// });
-
 gulp.task('default', function () {
     return gulp.src('src/assets/img/**/*')
         .pipe(webp())
         .pipe(gulp.dest('src/assets/img/webp'));
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('src/assets/css/src/*.css')
+gulp.task('default', function() {
+  return gulp.src(['src/dist/css/fonts.css' , 'src/dist/css/bootstrap.css' , 'src/assets/css/src/docs.css'])
+    .pipe(concatCss('style.css'))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/min'));
+    .pipe(gulp.dest('src/dist/css'));
 });
+
+gulp.task('default', function() {
+    return gulp.src(['src/assets/js/vendor/jquery.min.js' , 'src/dist/js/bootstrap.js','src/assets/js/docs.min.js', 'src/assets/js/ie10-viewport-bug-workaround.js'])
+        .pipe(concatJS('compress.js'))
+        .pipe(gulp.dest('src/dist/js'));
+});
+
+gulp.task('default', function (cb) {
+  pump([
+        gulp.src('src/dist/js/compress.js'),
+        uglify(),
+        gulp.dest('src/dist/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('image', function () {
+  gulp.src(['src/assets/img/*', 'src/assets/img/webp/*'])
+    .pipe(image())
+    .pipe(gulp.dest('src/assets/img/'));
+});
+
+gulp.task('default', ['image']);
